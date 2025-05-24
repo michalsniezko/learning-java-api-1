dbf4aa78dfe764f503f9d270d91cdf705b733f90
Michał Śnieżko <michalsniezko@gmail.com>
2025-05-24 21:23:20 +0200

    delete docker for now, add lombok

--- src/main/java/com/example/locations/model/Location.java
+++ src/main/java/com/example/locations/model/Location.java
@@ -1,64 +1,20 @@
 package com.example.locations.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.UUID;
 
+@Getter
+@Setter
 @Entity
 public class Location {
-
     @Id
-    private UUID id;
+    private UUID id = UUID.randomUUID();
     private String name;
     private String address;
     private String city;
     private double latitude;
     private double longitude;
-
-    public Location() {
-        this.id = UUID.randomUUID();
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
 }
